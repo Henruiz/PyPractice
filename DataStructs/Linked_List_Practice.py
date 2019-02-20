3f126cd75250b9c30380534d4223382570312f82
Henry <Hruiz1905@gmail.com>
2019-02-20 15:50:57 -0600

    Link List Example

---
+++ DataStructs/Linked_List_Practice.py
@@ -0,0 +1,31 @@
+Class Node:
+
+def init(self, value)
+    self.value = value
+    self.next = None
+
+
+class SLL:
+
+    def init(self)
+        self.head = None
+
+
+    def printLL(self)
+        print_value = value
+        if print_value is not None
+            print(print_value)
+            print_value = print.value.next
+
+
+    def add_at_front(self, value)
+        NewNode = Node(value)
+
+        NewNode.next = self.head
+        self.head = NewNode
+
+
+# def add_at_end(self, value)
+#     NewNode = Node(value)
+#
+#     NewNode.next =
