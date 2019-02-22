4ece47bf68aa5e325c3abc2b55249720f4dee49e
Henry <Hruiz1905@gmail.com>
2019-02-22 12:11:33 -0600

    Recursion Fibi

--- Practice_Problems/Fibinacci_Seq.py
+++ Practice_Problems/Fibinacci_Seq.py
@@ -1,21 +1,38 @@
-# try:
-user_input = int(input("Please enter a number from 1-100: "))
-# except:
-#     if user_input is not int:
-#         print('Sorry wrong input try again')
+global user_input
 
+counter = 0
+def fibinacci(num1, num2, counter):
+    counter += 1
+    if (num1 > num2):
+        return 1
+
+    if counter > user_input:
+        return ("Finished")
+
+    print(num1)
+
+    return fibinacci(num2, num1+num2, counter)
+
+
+user_input = int(input("Please enter a number from 1-100: "))
 num1 = 0
 num2 = 1
 
+fibinacci(num1, num2, counter)
 
-for i in range(user_input):
-    if i == 0:
-        print(num1, end=", ")
-    if i == 1:
-        print(num2, end=", ")
 
-    next = num1 + num2
-    num1 = num2
-    num2 = next
 
-    print(next, end=", ")
+#
+# for i in range(user_input):
+#     if i == 0:
+#         print(num1, end=", ")
+#     if i == 1:
+#         print(num2, end=", ")
+#
+#     next = num1 + num2
+#     num1 = num2
+#     num2 = next
+#
+#     print(next, end=", ")
+
+
