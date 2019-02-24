3655b10c4d8b791b9dc2a8712bc2b1014ac2427f
Henry <hruiz1905@gmail.com>
2019-02-24 12:09:46 -0600

    Windows Update

--- DataStructs/Linked_List_Practice.py
+++ DataStructs/Linked_List_Practice.py
@@ -4,78 +4,45 @@ class Node:
 
     # Function to initialise the node object
     def __init__(self, data):
-        self.data = data  # Assign data
-        self.next = None  # Initialize next as null
+        self.data = data
+        self.next = None
+        self.prev = None
 
 
 # Linked List class contains a Node object
 class LinkedList:
 
-    # Function to initialize head
     def __init__(self):
         self.head = None
 
     # Function to insert a new node at the beginning
     def insertFront(self, new_data):
-
-        # 1 & 2: Allocate the Node &
-        #        Put in the data
         new_node = Node(new_data)
-
-        # 3. Make next of new Node as head
         new_node.next = self.head
-
-        # 4. Move the head to point to new Node
         self.head = new_node
 
-        # This function is in LinkedList class. Inserts a
-
-    # new node after the given prev_node. This method is
-    # defined inside LinkedList class shown above */
+    # Function to insert a new node after current node
     def insertAfter(self, prev_node, new_data):
-
-        # 1. check if the given prev_node exists
         if prev_node is None:
             print
             "The given previous node must inLinkedList."
             return
-
-        #  2. create new node &
-        #      Put in the data
         new_node = Node(new_data)
-
-        # 4. Make next of new Node as next of prev_node
         new_node.next = prev_node.next
-
-        # 5. make next of prev_node as new_node
         prev_node.next = new_node
 
-        # This function is defined in Linked List class
-
-    # Appends a new node at the end.  This method is
-    # defined inside LinkedList class shown above */
+    # Function to insert a new node at the end
     def insertEnd(self, new_data):
-
-        # 1. Create a new node
-        # 2. Put in the data
-        # 3. Set next as None
         new_node = Node(new_data)
-
-        # 4. If the Linked List is empty, then make the
-        #    new node as head
         if self.head is None:
             self.head = new_node
             return
-
-        # 5. Else traverse till the last node
         last = self.head
         while(last.next):
             last = last.next
-
-        # 6. Change the next of last node
         last.next = new_node
 
-    # Function to add a node in the midde
+    # Function to add a node in the middle
     def insertMiddle(self, middle_node, newdata):
             if middle_node is None:
                 print("The mentioned node is absent")
@@ -104,10 +71,19 @@ class LinkedList:
 
         if head_value == None:
             return
-
         prev.next = head_value.next
-
         HeadVal = None
+
+    # Function to reverse the Linked List
+    def reverse(self):
+        prev = None
+        current = self.head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
 
     def printList(self):
         temp = self.head
@@ -138,4 +114,7 @@ llist.insertAfter(llist.head.next, 8)
 llist.insertMiddle(llist.head.next, 'middle')
 
 print('Created linked list is:',)
+llist.printList()
+print('Reversing the Linked List: ')
+llist.reverse()
 llist.printList()
